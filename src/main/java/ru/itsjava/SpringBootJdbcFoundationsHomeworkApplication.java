7b104e7d74632cdb4c5ae935b902420666fa137e
MichaelBash7 <bash-misha@mail.ru>
2022-03-06 09:11:56 +0300

    add Crud

--- src/main/java/ru/itsjava/SpringBootJdbcFoundationsHomeworkApplication.java
+++ src/main/java/ru/itsjava/SpringBootJdbcFoundationsHomeworkApplication.java
@@ -3,6 +3,10 @@ package ru.itsjava;
 import org.h2.tools.Console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import ru.itsjava.dao.UserDao;
+import ru.itsjava.domain.User;
+
 
 import java.sql.SQLException;
 
@@ -10,8 +14,26 @@ import java.sql.SQLException;
 public class SpringBootJdbcFoundationsHomeworkApplication {
 
     public static void main(String[] args) throws SQLException {
-        SpringApplication.run(SpringBootJdbcFoundationsHomeworkApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SpringBootJdbcFoundationsHomeworkApplication.class, args);
+
+//        Console.main(args);
+
+        UserDao userDao = context.getBean(UserDao.class);
+        System.out.println("userDao.count() = " + userDao.count());
+
+        User user = new User("Dima", 29);
+        userDao.insert(user);
+
+        System.out.println("userDao.count() = " + userDao.count());
+
+        User updatedUser = new User("Dima 2", 100);
+        updatedUser.setId(1L);
+        userDao.updateUser(updatedUser);
+
+        userDao.delete(updatedUser);
+        System.out.println("userDao.count() = " + userDao.count());
 
         Console.main(args);
+
     }
 }
