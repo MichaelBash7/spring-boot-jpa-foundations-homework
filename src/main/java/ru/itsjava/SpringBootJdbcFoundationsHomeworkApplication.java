257f1a0eacf4156541278f53b2fb02f50331c7fc
MichaelBash7 <bash-misha@mail.ru>
2022-03-09 18:21:40 +0300

    add UserJdbcDaoImplTest

--- src/main/java/ru/itsjava/SpringBootJdbcFoundationsHomeworkApplication.java
+++ src/main/java/ru/itsjava/SpringBootJdbcFoundationsHomeworkApplication.java
@@ -16,25 +16,6 @@ public class SpringBootJdbcFoundationsHomeworkApplication {
     public static void main(String[] args) throws SQLException {
         ApplicationContext context = SpringApplication.run(SpringBootJdbcFoundationsHomeworkApplication.class, args);
 
-//        Console.main(args);
-
-        UserDao userDao = context.getBean(UserDao.class);
-        System.out.println("userDao.count() = " + userDao.count());
-
-        User user = new User("Dima", 29);
-        userDao.insert(user);
-
-        System.out.println("userDao.count() = " + userDao.count());
-
-        User updatedUser = new User("Dima 2", 100);
-        updatedUser.setId(1L);
-        userDao.update(updatedUser);
-
-        System.out.println("studentDao.findById(1L) = " + userDao.findById(1L));
-
-        userDao.delete(updatedUser);
-        System.out.println("userDao.count() = " + userDao.count());
-
         Console.main(args);
 
     }
